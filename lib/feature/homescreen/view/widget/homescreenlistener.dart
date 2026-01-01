68a693d9757c3d70a3a82f0fad2daf27f32159c4
DESKTOP-5FT4BND\LCT <doaakhadejeh@gmail.com>
2026-01-01 14:01:17 -0800

    done

--- lib/feature/homescreen/view/widget/homescreenlistener.dart
+++ lib/feature/homescreen/view/widget/homescreenlistener.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:travel_planner/core/helper/dialoghelper.dart';
-import 'package:travel_planner/core/routing/const_rout.dart';
 import 'package:travel_planner/feature/homescreen/logic/cubit/homescreen_cubit.dart';
 import 'package:travel_planner/feature/homescreen/logic/cubit/homescreen_state.dart';
 
--- lib/feature/trip/view/trip.dart
+++ lib/feature/trip/view/trip.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:travel_planner/core/constant/imagesconst.dart';
 import 'package:travel_planner/feature/trip/logic/cubit/trip_cubit.dart';
 import 'package:travel_planner/feature/trip/logic/cubit/trip_state.dart';
 import 'package:travel_planner/feature/trip/view/widget/listenertrip.dart';
@@ -22,12 +21,6 @@ class Trip extends StatelessWidget {
         height: 700.h,
         width: double.infinity,
 
-        // decoration: BoxDecoration(
-        //   image: DecorationImage(
-        //     image: AssetImage(Imagesconst.home5),
-        //     fit: BoxFit.cover,
-        //   ),
-        // ),
         child: Column(
           children: [
             SizedBox(height: 20.h),
